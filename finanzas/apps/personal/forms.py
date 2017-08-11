61a610e3d551ed833191d1e17035aea05ab4244e
luistorresrw <luistorresrw@gmail.com>
2017-08-11 11:04:27 -0300

    formulaio personal

--- finanzas/apps/personal/forms.py
+++ finanzas/apps/personal/forms.py
@@ -1,2 +1,16 @@
+# -*- encoding: utf-8 -*-
 from django import forms
+from models import Ciudad, Sexo, Tipo_empleado, Agrupacion, Escalafon
 
+class EmpleadoForm (forms.Form):
+    apellido = forms.CharField(widget=forms.TextInput(attrs=dict({'class': 'form-control input-block-level', 'placeholder': 'Apellido'}, render_value=False)))
+    nombre = forms.CharField(widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Nombre'},render_value=False)))
+    dni = forms.CharField(widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'DNI'},render_value=False)))
+    domicilio = forms.CharField(widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Domicilio'},render_value=False)))
+    ciudad = forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level'})), queryset= Ciudad.objects.all())
+    email = forms.CharField(widget=forms.EmailInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'em@il'},render_value=False)))
+    sexo = forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level'})), queryset= Sexo.objects.all())
+    telefono = forms.CharField(widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Nro. teléfono'},render_value=False)))
+    tipo_empleado = forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level'})), queryset= Tipo_empleado.objects.all())
+    agrupacion = forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level'})), queryset= Agrupacion.objects.all())
+    escalafon = forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level'})), queryset= Escalafon.objects.all())
--- finanzas/apps/personal/views.py
+++ finanzas/apps/personal/views.py
@@ -3,10 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from finanzas.apps.personal.forms import EmpleadoForm
 
 
 def personal_create_view(request):
-    pass
+    form = EmpleadoForm()
+
+    values = {
+        'form':form
+    }
+    return render(request, 'formulario_empleado.html', values)
 
 def personal_read_view(request):
     pass
